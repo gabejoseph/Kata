2fc22bcd94a89f2398203f079f26e9356c88eae9
gabejoseph <gabriel.joseph4@gmail.com>
2021-01-11 10:47:28 -0500

    completed TenMinuteWalk.js

--- TenMinuteWalk.js
+++ TenMinuteWalk.js
@@ -1,6 +1,27 @@
 const isValidWalk = (walk) => {
     //insert brilliant code here
-  }
+    console.log(walk)
+  
+    let counterA = 0;
+    let counterB = 0;
+    if (walk.length !== 10) {
+      return false
+    }
+    
+    for (let i = 0; i < walk.length; i++) {
+        if (walk[i] === 'n') {
+          counterA++      
+        } else if (walk[i] === 's') {
+          counterA--
+        } else if (walk[i] === 'e') {
+          counterB++
+        } else if (walk[i] === 'w') {
+          counterB--
+        }
+    }
+    
+    return (counterA === 0 && counterB === 0) ? true : false
+}
 
 
 
