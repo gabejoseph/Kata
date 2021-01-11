7c3170f6edfc9e51d7af142073a4aef8aa3f31f8
gabejoseph <gabriel.joseph4@gmail.com>
2021-01-11 09:47:20 -0500

    TenMinuteWalk.js

---
+++ TenMinuteWalk.js
@@ -0,0 +1,13 @@
+const isValidWalk = (walk) => {
+    //insert brilliant code here
+  }
+
+
+
+
+
+//some test cases for you...
+console.log(isValidWalk(['n','s','n','s','n','s','n','s','n','s'])) //  'should return true'
+console.log(!isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e'])) // 'should return false'
+console.log(!isValidWalk(['w'])) // 'should return false'
+console.log(!isValidWalk(['n','n','n','s','n','s','n','s','n','s'])) // 'should return false'
